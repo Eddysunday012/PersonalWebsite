01cef03d13fa0fdc0eeb8a3e56dacfc43a970cd2
eddysunday012 <ethanddomingo@gmail.com>
2021-09-15 16:51:38 -0700

    Reworking projects page

--- EthanD_website/base/views.py
+++ EthanD_website/base/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def home(request):
     return render(request, 'base/home.html')
+
+def projects(request):
+    return render(request, 'base/projects.html')
