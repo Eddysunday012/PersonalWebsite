3bf05d71256d20028de7770140e992f017e6a878
Eddysunday012 <ethanddomingo@gmail.com>
2021-12-27 23:23:56 -0800

    added sent refresh html file

--- EthanD_website/sendemail/views.py
+++ EthanD_website/sendemail/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, redirect
+from django.core.mail import send_mail
 
 # Create your views here.
 
 def sendEmail(request):
-    return render(request, 'sendemail/email.html')
+    
+    if(request == "POST"):
+        send_mail()
+    
+    else:
+        return render(request, 'sendemail/email.html')
